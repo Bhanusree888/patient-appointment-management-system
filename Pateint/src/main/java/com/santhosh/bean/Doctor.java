ef5b950c63bd6fc3d310798fae598ae53687c1bb
santhosh <mallinenisanthosh1919@gmail.com>
2024-02-04 16:06:54 +0530

    sql modified

---
+++ Pateint/src/main/java/com/santhosh/bean/Doctor.java
@@ -0,0 +1,25 @@
+package com.santhosh.bean;
+public class Doctor {
+	private int d_id;
+	private String d_name;
+	private String d_role;
+	private String d_mblno;
+	private String d_email;
+	private String d_address;
+	private String d_image;
+	public void setd_Id(int d_id) {this.d_id=d_id;}
+	public void setd_Name(String d_name) {this.d_name=d_name;}
+	public void setd_Role(String d_role) {this.d_role=d_role;}
+	public void setd_Mblno(String d_mblno) {this.d_mblno=d_mblno;}
+	public void setd_Email(String d_email) {this.d_email=d_email;}
+	public void setd_Address(String d_address) {this.d_address=d_address;}
+	public void setd_Image(String d_image) {this.d_image=d_image;}
+	public int getd_Id() {return this.d_id;}
+	public String getd_Name() {return this.d_name;}
+	public String getd_Role() {return this.d_role;}
+	public String getd_Mblno() {return this.d_mblno;}
+	public String getd_Email() {return this.d_email;}
+	public String getd_Address() {return this.d_address;}
+	public String getd_Image() {return this.d_image;}
+}
+
