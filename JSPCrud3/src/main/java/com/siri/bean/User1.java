eb866a9a2d45ec4bfc421240602b95f5f5c68875
21hr1a0453 <135335543+21hr1a0453@users.noreply.github.com>
2024-02-06 09:41:31 +0530

    Add files via upload

---
+++ JSPCrud3/src/main/java/com/siri/bean/User1.java
@@ -0,0 +1,53 @@
+package com.siri.bean;
+
+public class User1{
+	private int pre_id;
+	private String doctor_name;
+	private String prescription1_type;
+	private String drugdosage;
+	private String pat_name;
+	private int quantity;
+	private String pres_date;
+	public int getPre_id() {
+		return pre_id;
+	}
+	public void setPre_id(int pre_id) {
+		this.pre_id = pre_id;
+	}
+	public String getDoctor_name() {
+		return doctor_name;
+	}
+	public void setDoctor_name(String doctor_name) {
+		this.doctor_name = doctor_name;
+	}
+	public String getPrescription1_type() {
+		return prescription1_type;
+	}
+	public void setPrescription1_type(String prescription1_type) {
+		this.prescription1_type = prescription1_type;
+	}
+	public String getDrugdosage() {
+		return drugdosage;
+	}
+	public void setDrugdosage(String drugdosage) {
+		this.drugdosage = drugdosage;
+	}
+	public String getPat_name() {
+		return pat_name;
+	}
+	public void setPat_name(String pat_name) {
+		this.pat_name = pat_name;
+	}
+	public String getPres_date() {
+		return pres_date;
+	}
+	public void setPres_date(String pres_date) {
+		this.pres_date = pres_date;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
